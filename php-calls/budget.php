82e0770c0994596b0fe27affa9a7b7b1d8767aad
Emily MaRee <lee@mleemaree.com>
2015-01-22 18:58:27 +0100

    entered cards into php calls

---
+++ php-calls/budget.php
@@ -0,0 +1,50 @@
+
+        <div id="budget" class="module card budget">
+          <nav>
+            <div class="top">
+                <span class="center"></span>
+                <img id="budget" src="images/budget.png">
+                <h2 class="modualHeader">Budget</h2>                
+            </div>
+        </nav>
+
+        
+        <section>
+            <div class="content">
+		        <div class="remaining">
+	                <h2>Remaining <strong>730€</strong> of <strong>1.300€</strong></h2>
+                    <span class="center"></span>
+                </div>
+
+        
+            <table>
+                <tr>
+                    <td class="cost">-24€</td>
+                    <td class="item">Gas / 24€ each</td>
+                </tr>
+                <tr>
+                    <td class="cost">-150€</th>
+                    <td class="item">Horse Riding / 30€ each</td>
+                </tr>
+                <tr>
+                    <td class="cost">-40€</td>
+                    <td class="item">Groceries / 8€ each</td>
+                </tr>
+                <tr>
+                    <td class="cost">-20€</td>
+                    <td class="item">Pizzas / 4€ each</td>	
+                </tr>
+            </table>
+            </div>  
+        </section>
+
+        <footer>
+            <div class="expand">
+                 <span class="center"></span>
+                 <img id="arrow" src="images/arrow.png">
+            </div>
+        </footer>
+        </div>
+      </article>
+   </section>
+
