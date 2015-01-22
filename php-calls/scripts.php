7afc044dc7fdecf94b7413ff78e69bd6f099487c
Emily MaRee <lee@mleemaree.com>
2015-01-22 20:35:28 +0100

    Css on planning page now looks normal though is not functional

--- php-calls/scripts.php
+++ php-calls/scripts.php
@@ -3,3 +3,4 @@
 <script src="assets/js/functions.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+<script src="assets/js/bring.js"></script>
